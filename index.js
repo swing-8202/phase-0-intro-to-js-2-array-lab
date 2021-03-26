07cfc75cbe62906199bc6da6bfabc8ff0ef936d1
Sokung <swing.sophie.8202@gmail.com>
2021-03-25 17:04:53 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name)
+}
+
+function appendCat(name) {
+    const newArray = [...cats];
+    newArray.push(name)
+    return newArray
+}
+
+function prependCat(name) {
+    const newArray = [...cats];
+    newArray.unshift(name)
+    return newArray
+}
+
+function removeLastCat(name) {
+    const newArray = [...cats];
+    newArray.pop(name)
+    return newArray
+}
+
+function removeFirstCat(name) {
+    const newArray = [...cats];
+    newArray.shift(name)
+    return newArray
+}
